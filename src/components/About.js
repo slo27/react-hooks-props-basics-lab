ad55b7ef249998d07224e06b0f69ef612489c957
sandy <sandyks.lo27@gmail.com>
2021-09-22 10:46:33 -0700

    commit tree 25999eb9c391a6a5ff456b21934dd44c3f41e536

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+
+  // function bioExists() {
+  //   if (!!props.bio) {
+  //     return (
+  //       <p>{props.bio}</p>
+  //     );
+  //   };
+  // };
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* {bioExists()} */}
+      {/* {props.bio ? <p>{props.bio}</p> : null} */}
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
